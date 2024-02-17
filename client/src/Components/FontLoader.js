add72a340fd51180242be052033f78eb5129da7d
jakebroffman <jake.broffman1088@gmail.com>
2024-02-17 09:36:34 -0500

    create FontLoader component

---
+++ client/src/Components/FontLoader.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Roboto_300Light, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@fontsource/roboto';
+
+const FontLoader = () => {
+    return (
+        <>
+            {Roboto_300Light}
+            {Roboto_400Regular}
+            {Roboto_500Medium}
+            {Roboto_700Bold}
+        </>
+    );
+};
+
+export default FontLoader;
